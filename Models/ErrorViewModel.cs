97992f2bae24162a995f0d220b9885e809bb1e87
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-10-04 23:25:58 +0300

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace todo_list_asp_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
