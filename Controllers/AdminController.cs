89d476cd523e3894db86d1c9fd12f49f3641bd9d
hasaslandev <126229388+hasaslandev@users.noreply.github.com>
2024-10-29 13:03:14 +0300

    Revert "Adding Layers"
    This reverts commit 37ca48800f8bf55b5d64ab807ce11f78290f648d.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace todo_list_asp_mvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _adminService.GetAll();
+            if (result.Success)
+            {
+                ViewBag.AdminList = result.Data;
+            }
+            return View();
+        }
+    }
+}
